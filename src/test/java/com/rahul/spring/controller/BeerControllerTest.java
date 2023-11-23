c07f3313ad12c6abab52e4a2bada390f6f7b8527
RahulReps <rahul.v2021c@vitstudent.ac.in>
2023-11-23 16:24:54 +0530

    add test method for get player by id

--- src/test/java/com/rahul/spring/controller/BeerControllerTest.java
+++ src/test/java/com/rahul/spring/controller/BeerControllerTest.java
@@ -1,7 +1,9 @@
 package com.rahul.spring.controller;
 
 import com.rahul.spring.controllers.PlayerController;
+import com.rahul.spring.model.Players;
 import com.rahul.spring.services.PlayerService;
+import com.rahul.spring.services.PlayerServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -11,9 +13,12 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.UUID;
 
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.willReturn;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.core.Is.is;
 @WebMvcTest(PlayerController.class)
 public class BeerControllerTest {
 
@@ -21,11 +26,19 @@ public class BeerControllerTest {
     MockMvc mockMvc;
     @MockBean
     PlayerService playerService;
+    PlayerServiceImpl playerServiceImpl = new PlayerServiceImpl();
+
 
     @Test
     void getPlayerById() throws Exception{
-        mockMvc.perform(get("/players/players/" + UUID.randomUUID())
+
+        Players players = playerServiceImpl.getAllPlayers().get(0);
+
+        given(playerService.getPlayerById(players.getId())).willReturn(players);
+
+        mockMvc.perform(get("/players/players/" + players.getId())
                 .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id", is(players.getId().toString())));
     }
 }
