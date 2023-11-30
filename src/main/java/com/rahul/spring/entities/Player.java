629981c2ca13f7b34f68d9d1c974926f9c6dc9ed
RahulReps <rahul.v2021c@vitstudent.ac.in>
2023-11-30 11:51:04 +0530

    created repository and its test splices

--- src/main/java/com/rahul/spring/entities/Player.java
+++ src/main/java/com/rahul/spring/entities/Player.java
@@ -1,8 +1,11 @@
 package com.rahul.spring.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
 
 import java.util.UUID;
 
@@ -14,6 +17,9 @@ import java.util.UUID;
 @Entity
 public class Player {
     @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID" , strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
     UUID id;
     String name;
     String position;
