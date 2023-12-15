58764bdf6e578acf0c20fd959f5edf2322d0378b
RahulReps <rahul.v2021c@vitstudent.ac.in>
2023-12-16 00:30:51 +0530

    configured my sql

--- src/main/java/com/rahul/spring/entities/Player.java
+++ src/main/java/com/rahul/spring/entities/Player.java
@@ -9,6 +9,8 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
 import java.util.UUID;
 
@@ -22,7 +24,8 @@ public class Player {
     @Id
     @GeneratedValue(generator = "UUID")
     @GenericGenerator(name = "UUID" , strategy = "org.hibernate.id.UUIDGenerator")
-    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+    @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
+    @JdbcTypeCode(SqlTypes.CHAR)
     UUID id;
     @NotBlank
     @NotNull
