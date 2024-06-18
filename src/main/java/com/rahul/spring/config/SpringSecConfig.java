c546977104d44c4d3d1b54b775984830a07835f5
RahulReps <rahul.v2021c@vitstudent.ac.in>
2024-06-19 00:16:38 +0530

    added actuators and others

--- src/main/java/com/rahul/spring/config/SpringSecConfig.java
+++ src/main/java/com/rahul/spring/config/SpringSecConfig.java
@@ -1,18 +1,33 @@
 package com.rahul.spring.config;
 
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Profile("!test") //in test profile this config won't be picked
 @Configuration
 public class SpringSecConfig {
 
     @Bean
+    @Order(1)
+    public SecurityFilterChain actuatorSecurityFilterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(authorize -> authorize.requestMatchers(EndpointRequest.toAnyEndpoint()).permitAll());
+
+        return http.build();
+    }
+
+    @Bean
+    @Order(2)
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorize -> {
-                    authorize.anyRequest().authenticated();
+                    authorize
+                            .requestMatchers("/v3/api-docs**", "/swagger-ui/**",  "/swagger-ui.html").permitAll()
+                            .anyRequest().authenticated();
                 }).oauth2ResourceServer(httpSecurityOAuth2ResourceServerConfigurer -> {
                     httpSecurityOAuth2ResourceServerConfigurer.jwt(Customizer.withDefaults());
                 });
---
+++ src/test/java/com/rahul/spring/controllers/PlayerControllerRestAssuredTest.java
@@ -0,0 +1,61 @@
+package com.rahul.spring.controllers;
+
+import com.atlassian.oai.validator.OpenApiInteractionValidator;
+import com.atlassian.oai.validator.restassured.OpenApiValidationFilter;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.test.context.ActiveProfiles;
+
+import static io.restassured.RestAssured.given;
+
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Import(PlayerControllerRestAssuredTest.TestConfig.class)
+@ComponentScan(basePackages =  "com.rahul.spring")
+public class PlayerControllerRestAssuredTest {
+
+    OpenApiValidationFilter filter = new OpenApiValidationFilter(OpenApiInteractionValidator
+            .createForSpecificationUrl("oa3.yml")
+            .build());
+
+    @Configuration
+    public static class TestConfig{
+        @Bean
+        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+            http.authorizeHttpRequests(authorize -> {
+                authorize.anyRequest().permitAll();
+            });
+
+            return http.build();
+        }
+    }
+
+    @LocalServerPort
+    Integer localPort;
+
+    @BeforeEach
+    void setUp(){
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = localPort;
+    }
+
+    @Test
+    void testListBeers(){
+        given().contentType(ContentType.JSON)
+                .when()
+                .filter(filter)
+                .get("/players/players")
+                .then()
+                .assertThat().statusCode(200);
+    }
+}
